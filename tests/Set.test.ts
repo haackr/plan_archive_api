a6131702797073640833ba41305236061a186ac8
Ryan Haack <ryan@haackr.com>
2020-12-20 11:44:13 -0700

    Finish up Set tests

--- tests/Set.test.ts
+++ tests/Set.test.ts
@@ -15,6 +15,8 @@ describe("Sets Tests", () => {
     expect(sets.setsData.length).toBeGreaterThan(0);
   });
 
+  let addedKey: number;
+
   it("lets the user add sets if they are logged in", async () => {
     await ctx.client.request(`
       mutation{
@@ -37,5 +39,107 @@ describe("Sets Tests", () => {
       }
     `);
     expect(newSet.createOneSetsData.ID).toEqual("19900513-550-NEW-R");
+    addedKey = newSet.createOneSetsData.Key;
+  });
+
+  it("lets the user modify sets if they are logged in", async () => {
+    const modifiedSet = await ctx.client.request(`
+      mutation {
+        updateOneSetsData(
+          where: {Key: ${addedKey}},
+          data: {
+            Title: {set: "Modified Title"}
+        }){
+          Title
+        }
+      }
+    `);
+    expect(modifiedSet.updateOneSetsData.Title).toBe("Modified Title");
+  });
+
+  it("lets the user delete sets if they are logged in", async () => {
+    const deletedSet = await ctx.client.request(`
+      mutation {
+        deleteOneSetsData(where: {
+          Key: ${addedKey}
+        }){
+          Title
+          Key
+          ID
+        }
+      }
+    `);
+    expect(deletedSet.deleteOneSetsData.Key).toBe(addedKey);
+
+    const setInDb = await ctx.db.setsData.findUnique({
+      where: { Key: addedKey },
+    });
+    expect(setInDb).toBeNull();
+  });
+
+  it("does not let the user do CUD operations when not logged in", async () => {
+    await ctx.client.request(`
+      mutation {
+        logout {
+          username
+        }
+      }
+    `);
+
+    let res;
+    try {
+      res = await ctx.client.request(`
+      mutation {
+        createOneSetsData(data: {
+          ID: "19900513-550-NEW-R",
+          Title: "Test Title",
+          Schools: {connect: {SchoolID: "123"}}
+        }){
+          ID
+          Key
+          Title
+        }
+      }
+    `);
+    } catch (error) {
+      res = error;
+    }
+    expect(res.response.errors[0].message).toContain("must be logged in");
+    res = null;
+
+    try {
+      res = await ctx.client.request(`
+      mutation {
+        updateOneSetsData(
+          where: {Key: ${addedKey}},
+          data: {
+            Title: {set: "Modified Title"}
+        }){
+          Title
+        }
+      }
+    `);
+    } catch (error) {
+      res = error;
+    }
+    expect(res.response.errors[0].message).toContain("must be logged in");
+    res = null;
+
+    try {
+      res = await ctx.client.request(`
+      mutation {
+        deleteOneSetsData(where: {
+          Key: 1
+        }){
+          Title
+          Key
+          ID
+        }
+      }
+    `);
+    } catch (error) {
+      res = error;
+    }
+    expect(res.response.errors[0].message).toContain("must be logged in");
   });
 });
